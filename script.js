046baf40fcb7df4dd0e8fee6eadb0fbbc2669dc7
unknown <jihanraw@gmail.com>
2025-08-29 06:58:02 +0600

    copy & call function added

---
+++ script.js
@@ -0,0 +1,22 @@
+// heart function start
+const navHeart = document.getElementById('nav-heart');
+const cardHearts = document.getElementsByClassName('card-heart');
+for(let card of cardHearts){
+    card.addEventListener("click", function(){
+        let new_heartValue = parseInt(navHeart.innerText);
+        navHeart.innerText = new_heartValue + 1;
+    })
+}
+// heart function end
+
+// copy function start
+const navCopy = document.getElementById('nav-copy');
+const cardCopy = document.getElementsByClassName('copy-card')
+for(let copy of cardCopy){
+    copy.addEventListener('click',function(){
+        let new_CopyValue = parseInt(navCopy.innerText);
+        navCopy.innerText = new_CopyValue + 1;
+    })
+}
+
+// copy function end
